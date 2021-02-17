70de0ce960b2f674755bf767c2342f18d6ff6d27
kennedy.ikeka <ikeka95@gmail.com>
2021-02-17 12:15:29 +0100

    Initial Commit

--- src/authentications/authentications.controller.ts
+++ src/authentications/authentications.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { Types } from 'mongoose';
 import { AuthenticationsService } from './authentications.service';
 import { AuthenticationDTO } from './dto/authentications.dto';
@@ -7,13 +7,19 @@ import { AuthenticationDTO } from './dto/authentications.dto';
 export class AuthenticationsController {
     constructor(private authenticationService: AuthenticationsService) { }
 
-    @Post()
-    create(@Body() data: AuthenticationDTO) {
-        return this.authenticationService.create(data);
+    @Get('')
+    find(@Query('id') id: Types.ObjectId) {
+        if(id) return this.authenticationService.findById(id);
+        return this.authenticationService.find();
     }
 
     @Get(':/id')
     findById(@Param('id') id: Types.ObjectId, @Body() data: AuthenticationDTO) {
+        return this.authenticationService.create(data);
+    }
+
+    @Post()
+    create(@Body() data: AuthenticationDTO) {
         return this.authenticationService.create(data);
     }
 }
